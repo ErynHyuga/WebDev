a77c5e3cf6929bfe421b55ee90d81539872437b9
ErynHyuga <you@example.com>
2024-11-04 16:26:05 +0100

    finished 01 &02

---
+++ 02_JS/01_JS_var/variablen.js
@@ -0,0 +1,22 @@
+console.log("Hallo ");
+console.log("Tutti");
+
+let a = 7;
+let b = 3;
+let c = a + b;
+console.log("C isch =" + c);
+console.log("A + B = " + c);
+c = b -a;
+con
+console.log("Neues C = " + C);
+a + b + c;
+console.log("Neues C = " + C);
+let f = (a + b) / c;
+console.log("(a + b) / c isch = " + f);
+c -a + b
+console.log("Neues C = " + C);
+a + b + c * 3
+console.log("Neues C = " + C);
+(a + b + c)/4
+console.log("Neues C = " + C);
+
---
+++ 02_JS/02_JS_Strings_und_Zahlen/strings.js
@@ -0,0 +1,16 @@
+let a = 7;
+let b = 3;
+let c = 7.4;
+let d = 7.5;
+
+let resulta = a + b;
+let resultb = c + d;
+
+console.log("a + b = " + resulta);
+console.log("c + d = " + resultb);
+
+let name = "Andi";
+
+console.log("Hallo " + name);
+
+console.log("Hallo " + name + " wie gehts?");

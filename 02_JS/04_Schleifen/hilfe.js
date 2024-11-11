dd357a2519f77fce9b3083ac38633561c4dffd85
ErynHyuga <you@example.com>
2024-11-11 16:26:14 +0100

    forgot the last part, now made it. Starting with 05

--- 02_JS/04_Schleifen/hilfe.js
+++ 02_JS/04_Schleifen/hilfe.js
@@ -14,3 +14,18 @@ for(let index = 10; index > 0 ; index--) {
 for(let index = 1234; index > 1207 ; index--) {
     console.log(index);
 }
+
+let name = "Min Vorname Und Nachname"
+
+for(let index = 0; index < 8 ; index++) {
+    console.log(name);
+}
+
+for(let index = 0; index < 10 ; index++) {
+    console.log(index);
+}
+console.log(name);
+
+for(let index = 10; index > 0 ; index--) {
+    console.log(index);
+}

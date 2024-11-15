a92c36d9e03905d14cbf4b8745630cc8cb5ef3a8
ErynHyuga <you@example.com>
2024-11-15 09:21:28 +0100

    installed node and finished 06

---
+++ 02_JS/05_If-clauses/script.js
@@ -0,0 +1,22 @@
+let x = 25;
+
+if (x <= 20) {
+    console.log("small");
+    
+}else if (20< x < 50) {
+    console.log("medium");
+
+}else if (50 <= x) {
+    console.log("large");
+    
+}
+
+let y = 45;
+
+if (y <= 7) {
+    console.log("small");
+}else if (7 < y < 90) {
+    console.log("medium");
+}else if (90 <= y) {
+    console.log("large");
+}
---
+++ 02_JS/06_Random/script.js
@@ -0,0 +1,5 @@
+let ranNum = Math.random();
+let roundNum = Math.floor(ranNum*10 + 20);
+
+console.log(roundNum);
+

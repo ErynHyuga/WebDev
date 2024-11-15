27811b0e8d448a3e819aa43b8b918496e61bc572
ErynHyuga <you@example.com>
2024-11-15 09:37:25 +0100

    finished 08switch

---
+++ 02_JS/08_switch/script.js
@@ -0,0 +1,24 @@
+let ranNum = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
+console.log(ranNum);
+
+switch (ranNum) {
+    case 6:
+        console.log("Zahl 6");
+        break;
+    case 7:
+        console.log("Zahl 7");
+        break;
+    case 8:
+        console.log("Zahl 8");
+        break;
+    case 9:
+        console.log("Zahl 9");
+        break;
+    case 10:
+        console.log("Zahl 10");
+        break;
+    default:
+        console.log("Zahl nicht zwischen 4 und 10");
+        break;
+
+}

9c7383f894e51c5dea7508c8ba6a485031002925
ErynHyuga <you@example.com>
2024-11-18 16:00:09 +0100

    finished 09

---
+++ 02_JS/09_zeichen/script.js
@@ -0,0 +1,18 @@
+let a = 8;
+let b = 2;
+
+console.log(a % 2);
+
+if(a % b == 0) {
+    console.log("number is even");
+}else{
+    console.log("number is odd");
+    
+}
+
+for(let i = 0; i < 100; i++) {
+    console.log(i);
+    if (i%2 == 0) {
+        console.log("");
+    }
+}

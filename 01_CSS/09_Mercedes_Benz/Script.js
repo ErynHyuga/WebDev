3781f60c0016be494e93c30ed5a97f3a98dbc42a
ErynHyuga <you@example.com>
2024-12-02 15:54:55 +0100

    coded a bit on the tdot day, and let the ai code a bit

--- 01_CSS/09_Mercedes_Benz/Script.js
+++
@@ -1,7 +0,0 @@
-function more(){
-    alert("Mehr Informationen auf dieser Seite" + "\n" + "https://www.mercedes-benz.de/passengercars.html");
-    
-}
-function demand(){
-    alert("Angebot wurde geschickt");
-}
--- 02_JS/11_arrays/script.js
+++ 02_JS/11_arrays/script.js
@@ -1,12 +1,15 @@
-let arList = [4,1,2,3];
+const arList = [4, 1, 2, 3];
 console.log(arList);
 
 arList.push(17, 199);
 console.log(arList);
-let o = 0;
-for (let i = 0; i < arList.length; i++) {
-    o = arList[i] ;
-    o += arList[i];
-    console.log(o);
-    
-}
+
+const sum = arList.reduce((acc, curr) => acc + curr, 0);
+console.log(sum);
+
+const freunde = ["Susi", "Paula", "Hans"];
+
+const freundesliste = () => freunde.join(", ");
+
+console.log(`Meine Freunde sind ${freundesliste()}`);
+

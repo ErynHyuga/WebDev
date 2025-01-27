91f8ccbaa7b7dfd000d11ee33d26fe7994680ba6
ErynHyuga <you@example.com>
2025-01-27 15:24:06 +0100

    finished main function of the 01 Calculator

--- 03_HTML+JS/01_calculator/script.js
+++ 03_HTML+JS/01_calculator/script.js
@@ -1,47 +1,53 @@
 euqation = "";
 
+
 function add() {
-  euqation = "add";
-  console.log("add");
-  
+    euqation = "add";
+    console.log("add");
+
 }
-function sub(){
+function sub() {
     euqation = "sub";
     console.log("sub");
-    
-}   
-function mul(){
+
+}
+function mul() {
     euqation = "mul";
     console.log("mul");
 }
-function div(){
+function div() {
     euqation = "div";
     console.log("div");
-    
+
 }
-function calCER(){
-    switch(euqation){
-    case "add":
-        result = num1 + num2;
-        return result;
-    case "sub":
-        result = num1 - num2;
-        return result;
-    case "mul":
-        result = num1 * num2;
-        return result;
-    case "div":
-        result = num1 / num2;
-        return result;
-}}
+function calCER() {
+    let str1 = document.getElementById("num1").value;
+    let str2 = document.getElementById("num2").value;
+    let num1 = Number(str1);
+    let num2 = Number(str2);
+    switch (euqation) {
+        case "add":
+            result = num1 + num2;
+            return result;
+        case "sub":
+            result = num1 - num2;
+            return result;
+        case "mul":
+            result = num1 * num2;
+            return result;
+        case "div":
+            result = num1 / num2;
+            return result;
+    }
+    buttonClick();
+}
 
 
 async function buttonClick() {
-    let num1 = document.getElementById("num1").value;
-    let num2 = document.getElementById("num2").value;
 
-    
-    document.getElementById("result").innerHTML = "The result is " + await calCER();    
+    calCER();
+    console.log("button clicked");
+
+    document.getElementById("result").innerHTML = "The result is " + result;
+    console.log(result);
 }
-
-

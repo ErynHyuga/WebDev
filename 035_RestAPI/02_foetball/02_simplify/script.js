0c36123c2590b211bbd1cf48d6d11f67cf25060d
ErynHyuga <you@example.com>
2025-03-03 15:47:12 +0100

    02_2 finished.

--- 035_RestAPI/02_foetball/02_simplify/script.js
+++ 035_RestAPI/02_foetball/02_simplify/script.js
@@ -1,8 +1,6 @@
-document.getElementById("table").addEventListener("click", function() {
-    let selectedTeamId = event.target.getAttribute("data-teamId");
-    console.log("Auswahl: " + selectedTeamId);
-    
-});
+
+
+
 
 const loadDataFromAPI = () => {
     fetch("https://api.openligadb.de/getbltable/bl1/2023")
@@ -11,15 +9,32 @@ const loadDataFromAPI = () => {
             console.log(json);
             let html = "<div id='content'>";
             for (let i = 0; i < json.length; i++) {
-                html += `<div data-teamId="${json[i].teamInfoId}" >${json[i].teamName}</div>`;
-                html += `<div>${json[i].points}</div>`;
-                html += `<div><img src="${json[i].teamIconUrl}" width="100px"></div>`;
+                html += `<div data-teamId="${json[i].teamInfoId}"><div><img src="${json[i].teamIconUrl}" width="100px"></div>`;
+                html += `<div>${json[i].teamName}</div>`;
+                html += `<div>${json[i].points}</div></div>`;
             }
             html += "</div>";
             document.getElementById("content").innerHTML = html;
         });
 }
-document.getElementById("table").addEventListener("click", (event) => {
-    alert("isndie");
+document.getElementById("content").addEventListener("click", (event) => {
+    let selectedTeamId = event.target.getAttribute("data-teamId");
+    console.log("Selected Team: ", selectedTeamId);
+    getNextMatchForTeam(selectedTeamId);
 });
+
+const getNextMatchForTeam = (teamId) => {
+    fetch(`https://api.openligadb.de/getnextmatchbyleagueteam/4741/${teamId}`)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      let html = "<div id='nextMatch'>";
+      html += `<div>${json.matchDateTime}</div>`;
+  
+      html += `<div><img width="50px" src="${json.team1.teamIconUrl}"/>${json.team1.teamName} vs. ${json.team2.teamName}<img width="50px" src="${json.team2.teamIconUrl}"/></div>`;
+      html += "</div>";
+      document.getElementById("nextMatch").innerHTML = html;
+    })
+  }
+  
 loadDataFromAPI();
